7b922ea4078b46a801189d079059cf9e159c51fc
Vitaliy <vitalik201444@gmail.com>
2020-04-24 12:27:40 +0500

    init

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      useBuiltIns: 'usage',
+      corejs: 3,
+      // debug: true,
+      // exclude: ['es6.regexp.to-string', 'es6.number.constructor'],
+    }],
+    "@babel/preset-typescript",
+    "@babel/preset-react"
+  ],
+
+  plugins: ["@babel/plugin-proposal-class-properties"]
+}
